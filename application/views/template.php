8b43b0a8f9a42231b13fd516116cf18822d6d07f
Андрей <andrey.buharov@gmail.com>
2026-03-09 09:04:08 +0300

    template.php убрал все лишнее и повторяющееся.

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Artonit City - панель управления СКУД Артонит">
+    <meta name="description" content="Artonit City - панель управления СКУД Артонит">
     <meta name="author" content="www.artonit.ru">
     <link rel="shortcut icon" href="/city/favicon.ico">
 
@@ -22,80 +22,67 @@
     ?>
     </title>
 
-    <!-- Bootstrap core CSS -->
-    <?php echo HTML::style('static/css/bootstrap.css'); ?>
-	<?php echo HTML::style('static/css/modal.css'); ?>
-   
-	<?php echo HTML::style('static/css/city.css'); ?>
-    <!--<?php echo HTML::style('static/css/bootstrap.css'); ?>-->
-    <?php echo HTML::style('static/css/modal.css'); ?>
-    <?php echo HTML::style('static/css/city.css'); ?>
-	
-	<link rel="stylesheet" type="text/css" media="all" href="/city/static/css/theme.blue.css">
-	<link rel="stylesheet" type="text/css" media="all" href="/city/static/css/jquery.dataTables.min.css">
-	<link rel="stylesheet" type="text/css" media="all" href="/city/static/css/theme.default.min.css">
-	<link rel="stylesheet" href="/city/static/css/themes/blue/style.css" type="text/css" media="print, projection, screen" />
-	 
-<!-- JavaScript -->
-	
-    <script type="text/javascript" src="/city/static/js/jquery-2.2.4.js"></script>
-    <script type="text/javascript" src="/city/static/js/moment-with-locales.min.js"></script>
-    <script type="text/javascript" src="/city/static/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/city/static/js/bootstrap-datetimepicker.min.js"></script>
-    <script type="text/javascript" src="/city/static/js/jquery.tablesorter.js"></script>
-    <script type="text/javascript" src="/city/static/js/jquery.tablesorter.widgets.js"></script>
-    <script type="text/javascript" src="/city/static/js/jquery.tablesorter.pager.js"></script>
-    <script type="text/javascript" src="/city/static/js/crm2_template_tablesorter.js"></script>
-	 
-    <link rel="stylesheet" type="text/css" media="all" href="/city/static/css/theme.blue.css">
-    <link rel="stylesheet" type="text/css" media="all" href="/city/static/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type="text/css" media="all" href="/city/static/css/theme.default.min.css">
-    <link rel="stylesheet" href="/city/static/css/themes/blue/style.css" type="text/css" media="print, projection, screen" />
+    <!-- CSS - ТОЛЬКО НЕОБХОДИМОЕ -->
+    <link rel="stylesheet" href="/city/static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/city/static/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="/city/static/css/modal.css">
+    <link rel="stylesheet" href="/city/static/css/city.css">
     
-    <!-- 5. Подключить CSS платформы Twitter Bootstrap 3 -->  
-    <link rel="stylesheet" href="/city/static/css/bootstrap.min.css" />
-    <!-- 6. Подключить CSS виджета "Bootstrap datetimepicker" -->  
-    <link rel="stylesheet" href="/city/static/css/bootstrap-datetimepicker.min.css" />
+    <!-- Tablesorter theme (только один) -->
+    <link rel="stylesheet" href="/city/static/css/theme.blue.css">
+    
+    <!-- JavaScript в HEAD (только критический) -->
+    <script src="/city/static/js/jquery-2.2.4.js"></script>
 </head>
 
 <body>
-   <div class="container">
+    <div class="container">
         <div class="row">
-
-		<span id="time-top"></span>
+            <span id="time-top"></span>
+            
             <?php
-            // Подготавливаем данные для меню
-			$menu_data = array(
-				'menu_active' => Arr::get($_SESSION, 'menu_active', ''), // Текущий активный пункт
-				'config' => Kohana::$config->load('artonitcity_config'), // Конфигурация
-				'logged_in' => Auth::instance()->logged_in(),            // Статус авторизации
-				'user' => Auth::instance()->get_user(),                    // Данные пользователя
-				'view_without_auth' => (array) $config->get('view_without_auth', array())// список пунктов меню без авторизации
-			);
+            // Подготовка данных для меню
+            $menu_data = array(
+                'menu_active' => Arr::get($_SESSION, 'menu_active', ''),
+                'config' => $config,  // Используем уже загруженный конфиг
+                'logged_in' => Auth::instance()->logged_in(),
+                'user' => Auth::instance()->get_user(),
+                'view_without_auth' => (array) $config->get('view_without_auth', array())
+            );
             
-            // Безопасно подключаем меню через View::factory
             echo View::factory('top_menu', $menu_data)->render();
-            
-            // Основной контент
             echo $content;
             ?>
+            
             <button onclick="topFunction()" id="myBtn" title="<?php echo __('top'); ?>">
                 <?php echo __('top'); ?>
             </button>
         </div>
     </div>
-<span id="time-bottom" style="display:none;">Страница подготовлена за <?php echo round(microtime(TRUE) - START_TIME, 3); ?> сек.</span>
     
+    <span id="time-bottom" style="display:none;">Страница подготовлена за <?php echo round(microtime(TRUE) - START_TIME, 3); ?> сек.</span>
 
-    <script type="text/javascript">
+    <!-- JavaScript в конце body (для скорости загрузки) -->
+    <script src="/city/static/js/moment-with-locales.min.js"></script>
+    <script src="/city/static/js/bootstrap.min.js"></script>
+    <script src="/city/static/js/bootstrap-datetimepicker.min.js"></script>
+    <script src="/city/static/js/jquery.tablesorter.js"></script>
+    <script src="/city/static/js/jquery.tablesorter.widgets.js"></script>
+    <script src="/city/static/js/crm2_template_tablesorter.js"></script>
+    
+    <!-- Опционально: tablesorter.pager только если нужна пагинация -->
+    <?php if (isset($use_pager) && $use_pager): ?>
+        <script src="/city/static/js/jquery.tablesorter.pager.js"></script>
+    <?php endif; ?>
+
+    <script>
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("myBtn").style.display = "block";
-        } else {
-            document.getElementById("myBtn").style.display = "none";
-        }
+        var btn = document.getElementById("myBtn");
+        if (!btn) return;
+        btn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+            ? "block" : "none";
     }
 
     function topFunction() {
@@ -103,14 +90,13 @@
         document.documentElement.scrollTop = 0;
     }
 
-
-	document.addEventListener('DOMContentLoaded', function() {
-		var bottomTime = document.getElementById('time-bottom');
-		var topTime = document.getElementById('time-top');
-		if (bottomTime && topTime) {
-			topTime.textContent = bottomTime.textContent;
-		}
-	});	
+    document.addEventListener('DOMContentLoaded', function() {
+        var bottomTime = document.getElementById('time-bottom');
+        var topTime = document.getElementById('time-top');
+        if (bottomTime && topTime) {
+            topTime.textContent = bottomTime.textContent;
+        }
+    });
     </script>
 </body>
 </html>
