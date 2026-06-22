1d5f4aae86de9e4f5892b64278e51f8f406c1007
Андрей <andrey.buharov@gmail.com>
2026-06-22 08:46:27 +0300

    Пробую сделать несколько настроек для вывода меню.

---
+++ application/config/adm.php
@@ -0,0 +1,46 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+    // Базовые пункты, которые всегда есть
+    'config' => array(
+        'title' => 'Артонит Сити Конфигуратор',
+        'url' => '/',
+        'icon' => 'fa-home',
+        'order' => 0,
+		
+
+    ),
+	'baselist' => array(
+        'title' => 'Справочники',
+        'url' => 'dashboard/log',
+        'icon' => 'fa-home',
+        'order' => 50,
+		'disabled' => false, 
+		 'children' => array(
+            'tasks' => array(
+                'title' => 'Тип ТС',
+                'url' => 'bas'
+            ),
+            'setting' => array(
+                'title' => 'Тип идентификаторов',
+                'url' => 'bas'
+            ),
+			'config' => array(
+                'title' => 'Типы устройств',
+                'url' => 'bas/search'
+            )
+			
+        )
+		
+    ),
+	'devgroup' => array(
+        'title' => 'Группы устройств',
+        'url' => '/',
+        'icon' => 'fa-home',
+        'order' => 0,
+		
+
+    ),
+	
+	
+);
