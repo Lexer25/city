624b95c465f84a46ba959610da451c532c22c457
Андрей <andrey.buharov@gmail.com>
2026-06-23 10:10:57 +0300

    версия берется из artonitcity_config.php... И тут исправил версию.

--- application/config/artonitcity_config.php
+++ application/config/artonitcity_config.php
@@ -10,8 +10,8 @@ return array(
     'dir_compare' => 'C:\\xampp\\htdocs\\city',
     'stat_day_befor' => 2,
     'city_name' => 'ЖК Хедлайнер',
-    'ver' => '3.0.0',
-    'timeUpdate' => '2026-06-03',
+    'ver' => '3.0.1',
+    'timeUpdate' => '2026-06-23',
     'lightVerDay' => 3,
     'developer' => 'www.artonit.ru',
     'main_windows' => array(
